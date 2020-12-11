731ca31537fa4a65a8c6a7d5278cb4b0fb2dd88a
TeacH <teach.inbox@gmail.com>
2020-12-11 21:01:27 +0100

    chatApp composer impl

--- src/components/chatapp/components/messageComposer/index.tsx
+++ src/components/chatapp/components/messageComposer/index.tsx
@@ -1,9 +1,11 @@
 import React, {useRef, useState} from 'react';
 import firebase from "firebase";
+import SendIcon from '@material-ui/icons/Send';
 import './index.scss'
+import {TextField} from "@material-ui/core";
 
 export const MessageComposer: React.FC = () => {
-    const [formValue, setFormValue] = useState('');
+    const [inputMessage, setInputMessage] = useState('');
     const bottomSpan = useRef();
     const firestore = firebase.firestore();
     const messagesRef = firestore.collection('messages');
@@ -11,29 +13,45 @@ export const MessageComposer: React.FC = () => {
 
     const sendMessage = async (e: any) => {
         e.preventDefault();
-        if(formValue === "") return;
+        if(inputMessage === "") return;
         // @ts-ignore
         const { uid, photoURL } = auth.currentUser;
 
         await messagesRef.add({
-            text: formValue,
+            text: inputMessage,
             createdAt: firebase.firestore.FieldValue.serverTimestamp(),
             uid,
             photoURL
         })
 
-        setFormValue('');
+        setInputMessage('');
         //current.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    const sendIconColor = () => {
+        return inputMessage === "" ? '#A9A9A9' : '#25d366';
+
     }
 
     return(
         <div className={'messageComposer_con'}>
-            <input
-                value={formValue}
-                onChange={(e) => setFormValue(e.target.value)}
-                placeholder="Type a message"
+            <TextField
+                id="standard-multiline-flexible"
+                label="Message"
+                multiline
+                rowsMax="3"
+                value={inputMessage}
+                onChange={(e) => setInputMessage(e.target.value)}
+                placeholder={"Type a message..."}
+
             />
-            <button onClick={(e) => sendMessage(e)}>Send</button>
+            <div className={'messageComposer_sendIcon'}>
+            <SendIcon
+                onClick={(e) => sendMessage(e)}
+                color={'inherit'}
+                style={{color: sendIconColor()}}
+            />
+            </div>
         </div>
     );
 }
