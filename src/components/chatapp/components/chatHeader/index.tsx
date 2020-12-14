dea65ad504d5cea95ea4519ec3e2e5388664cdc8
TeacH <teach.inbox@gmail.com>
2020-12-14 22:09:38 +0100

    avatar header impl

--- src/components/chatapp/components/chatHeader/index.tsx
+++ src/components/chatapp/components/chatHeader/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import firebase from "../../../../modules/firebase";
 import "./index.scss";
 import { SignOut } from "../signOut";
+import {Avatar} from "../../../avatar";
 
 export const ChatHeader: React.FC = () => {
-
+    const user = firebase.getUser();
+    console.log('USER', user);
+    const photoUrl = user ? user.photoURL : '';
     return(
             <div className={'chatHeader_con'}>
-                <div className={'chatHeader_empty'}></div>
+                <div className={'chatHeader_empty'}><Avatar url={photoUrl}/></div>
                 <div className={'chatHeader_title'}>Chat App</div>
                 <div className={'chatHeader_options'}>{<SignOut/>}</div>
             </div>
