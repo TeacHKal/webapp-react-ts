374c1d436f024bcea7bf835db9faa55dca52b2e5
TeacH <teach.inbox@gmail.com>
2020-12-21 17:46:39 +0100

    chatApp chatHeader btn impl

--- src/components/chatapp/components/chatHeader/index.tsx
+++ src/components/chatapp/components/chatHeader/index.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import firebase from "../../../../modules/firebase";
-import "./index.scss";
-import { SignOut } from "../signOut";
 import {Avatar} from "../../../avatar";
+import language from '../../../../config/languages';
+import {Button} from "../../../baseComponents/button";
+import "./index.scss";
 
 export const ChatHeader: React.FC = () => {
+    const strings = language.strings.chatApp;
     const user = firebase.getUser();
     const photoUrl = user ? user.photoURL : '';
+
+    const onSignInClick = () => {
+        firebase.signOut();
+    }
+
+    const renderSignInBtn = () => {
+        return (
+            <div>
+                <Button
+                    text={strings.signOut}
+                    onClick={() => onSignInClick()}
+                    variant="contained"
+                    color="secondary"
+                    size="small"
+                    disabled={!user}
+                >
+                </Button>
+            </div>
+        );
+    }
+
     return(
             <div className={'chatHeader_con'}>
                 <div className={'chatHeader_empty'}><Avatar url={photoUrl}/></div>
-                <div className={'chatHeader_title'}>Chat App</div>
-                <div className={'chatHeader_options'}>{<SignOut/>}</div>
+                <div className={'chatHeader_title'}>{strings.chatApp}</div>
+                <div className={'chatHeader_options'}>{renderSignInBtn()}</div>
             </div>
     );
 }
