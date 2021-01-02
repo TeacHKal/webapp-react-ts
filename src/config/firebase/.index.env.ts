ab637570993d5a39623755f55dd365a0a4e9ba12
TeacH <teach.inbox@gmail.com>
2021-01-02 05:05:46 +0100

    chatApp firebase cfg example

--- src/config/firebase/.index.env.ts
+++ src/config/firebase/.index.env.ts
@@ -1,10 +1,11 @@
 // example data
 export const firebaseConfig = {
-    apiKey: "apiKey",
-    authDomain: "authDomain",
-    projectId: "projectId",
-    storageBucket: "storageBucket",
-    messagingSenderId: "messagingSenderId",
-    appId: "appId",
-    measurementId: "measurementId"
+    apiKey: "API_KEY",
+    authDomain: "PROJECT_ID.firebaseapp.com",
+    databaseURL: "https://PROJECT_ID.firebaseio.com",
+    projectId: "PROJECT_ID",
+    storageBucket: "PROJECT_ID.appspot.com",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID",
+    measurementId: "G-MEASUREMENT_ID",
 };
