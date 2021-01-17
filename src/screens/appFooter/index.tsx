35cbe68c8cc64eb649447aa6b07accfe33990ee3
TeacH <teach.inbox@gmail.com>
2021-01-17 13:36:19 +0100

    underConstruction div

--- src/screens/appFooter/index.tsx
+++ src/screens/appFooter/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './index.scss';
+import WarningIcon from '@material-ui/icons/Warning';
 
 export const AppFooter: React.FC = () => {
 
     return(
         <div className={'footer_con'}>
-            <div className={'div_underContstruct'}>
+            <div className={'underConstruction_div'}>
+                <div className={'underConstruction_icon'}>
+                    <WarningIcon color="error" />
+                </div>
                 SITE UNDER CONSTRUCT
+                <div className={'underConstruction_icon'}>
+                    <WarningIcon color="error" />
+                </div>
             </div>
         </div>
     );
