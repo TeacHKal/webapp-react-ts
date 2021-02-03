b42cfaf3f8b5b8e731b60a53469dcb394f74d909
TeacH <teach.inbox@gmail.com>
2021-02-03 13:01:39 +0100

    remove unnecessary commented functions

--- src/features/todos/epics.ts
+++ src/features/todos/epics.ts
@@ -1,49 +1,20 @@
 import { from, of } from 'rxjs';
 import { filter, switchMap, map, catchError } from 'rxjs/operators';
 import { MyEpic } from '../../store/configureStore';
-import { getTodosRequest, getTodosFailure, getTodosSuccess } from '../../features/todos/reducer';
+import { getTodosRequest, getTodosFailure, getTodosSuccess } from './reducer';
 import { Todo } from "MyModels";
 import { getTodos } from "./actions";
-
-
 
 export const getTodosEpic: MyEpic = (action$, state$, { api }) =>
     action$.pipe(
         filter(getTodos.match),
         switchMap(() =>
             from(api.todosService.getTodos()).pipe(
-                map((payload: any) => getTodosSuccess(payload)),
+                map((payload: any) => {
+                    return getTodosSuccess(payload)
+                }),
                 catchError((message: string) => {
-                    console.log('getTodosEpic Error', message || message)
                     return of(getTodosFailure())})
             )
         )
     );
-
-// export const loadTodosEpic: Epic<RootAction, RootAction, RootState, Services>
-//     = (action$, state$, { api }) =>
-//   action$.pipe(
-//     filter(isActionOf(loadTodosAsync.request)),
-//     switchMap(() =>
-//       from(api.todosService.loadSnapshot()).pipe(
-//         map(loadTodosAsync.success),
-//         catchError((message: string) => of(loadTodosAsync.failure(message)))
-//       )
-//     )
-//   );
-
-//
-// export const saveTodosEpic: MyEpic = (action$, state$, { api }) =>
-//   action$.pipe(
-//     filter(isActionOf(saveTodosAsync.request)),
-//     switchMap(() =>
-//         // @ts-ignore
-//       from(api.todos.saveSnapshot(getTodos(state$.value.todos))).pipe(
-//         map(saveTodosAsync.success),
-//         catchError((message: string) => of(saveTodosAsync.failure(message)))
-//       )
-//     )
-//   );
-
-
-
