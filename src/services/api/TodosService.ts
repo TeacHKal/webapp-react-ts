72fe5eda03ad9dda555290c05d8b8e8ab7644544
TeacH <teach.inbox@gmail.com>
2021-02-03 13:02:04 +0100

    fix payload

--- src/services/api/TodosService.ts
+++ src/services/api/TodosService.ts
@@ -1,12 +1,6 @@
 import { map } from "rxjs/operators";
 import { Todo } from "MyModels";
 
-const todos: Todo[] = [
-    { id: 0, name: `Hustle Everyday :)`, isComplete: true },
-    { id: 1, name: `Learn Something New`, isComplete: false },
-    { id: 2, name: `Howdy Howdy`, isComplete: true },
-];
-
 export default class TodosService {
     readonly service;
 
@@ -14,35 +8,16 @@ export default class TodosService {
         this.service = service
     }
 
-
     getTodos() {
         return this.service
             .fetch$({
                 url: 'http://www.mocky.io/v2/5e5592ed31000033b7eb3930?mocky-delay=2000ms',
                 method: 'get'
             })
-            .pipe(map((result: any) => {
-                console.log('RESULT', result);
-                    return result.data
+            .pipe(map((result: Array<Todo>) => {
+                    return result
                 }
             ));
     }
 
-
-    loadSnapshot(): Promise<Todo[]> {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(todos);
-            }, 500);
-        });
-    }
-
-// saveSnapshot(data: Todo[]): Promise<undefined> {
-//         return new Promise((resolve, reject) => {
-//             setTimeout(() => {
-//                 todos = data;
-//                 //resolve();
-//             }, 500);
-//         });
-//     }
 }
