0091e1cb63b681def3e510fc278be7fbf62ec50c
TeacH <teach.inbox@gmail.com>
2021-02-03 15:02:18 +0100

    code clean

--- src/features/todos/epics.ts
+++ src/features/todos/epics.ts
@@ -2,14 +2,11 @@ import { from, of } from 'rxjs';
 import { filter, map, catchError, exhaustMap } from 'rxjs/operators';
 import { MyEpic } from '../../store/configureStore';
 import { getTodosRequest, getTodosFailure, getTodosSuccess } from './reducer';
-import { Todo } from "MyModels";
 import { getTodosRequestAction } from "./actions";
-
 
 export const getTodosEpic: MyEpic = (action$, state$, { api }) =>
     action$.pipe(
         filter(getTodosRequestAction.match),
-        map(() => getTodosRequest()),
         exhaustMap(() =>
             from(api.todosService.getTodos()).pipe(
                 map((payload: any) => {
@@ -19,5 +16,7 @@ export const getTodosEpic: MyEpic = (action$, state$, { api }) =>
                     console.log('getTodosRequest Error', message);
                     return of(getTodosFailure())})
             )
-        )
+        ),
     );
+
+
